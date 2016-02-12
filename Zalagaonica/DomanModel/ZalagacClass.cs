6a1557c9acade160aeded6cf566f3e3998c12d89
ivica94 <ivicastanimirovic@gmail.com>
2016-02-12 20:33:55 +0100

    idem po parohiju

--- Zalagaonica/DomanModel/ZalagacClass.cs
+++ Zalagaonica/DomanModel/ZalagacClass.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Zalagaonica.DomanModel
 {
     public class ZalagacClass
     {
+        public ObjectId id;
+        public String ime;
+        public String prezime;
+
     }
 }

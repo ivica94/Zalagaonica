64702d41ca6578fabb29e70ca507c47bb2a1f09f
ivica94 <ivicastanimirovic@gmail.com>
2016-02-13 13:37:05 +0100

    trololol

---
+++ Zalagaonica/DodavanjeRadnje.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using Zalagaonica.DomanModel;
+namespace Zalagaonica
+{
+    public partial class DodavanjeRadnje : Form
+    {
+        public DodavanjeRadnje()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var database = server.GetDatabase("Zalagaonica");
+            var collection = database.GetCollection<RadnjaClass>("radnja");
+            RadnjaClass radnja1=new RadnjaClass {
+                broj=Int32.Parse(textBoxBrojIspostave.Text), 
+                adresa=textBoxAdresa.Text, 
+                radnici = new List<MongoDBRef>() 
+            };
+            collection.Insert(radnja1);
+            foreach (RadnjaClass rad in collection.FindAll())
+            {
+                MessageBox.Show(rad.broj.ToString());
+            }
+            textBoxAdresa.Text = "";
+            textBoxBrojIspostave.Text = "";
+        }
+    }
+}

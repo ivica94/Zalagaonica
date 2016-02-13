6c984da4879cbc5bfd4133c5734313d9ff3715a3
sindja <milansi@stud.ntnu.no>
2016-02-13 13:29:00 +0100

    pocet vlasnik

---
+++ Zalagaonica/DodavanjeRadnje.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Zalagaonica
+{
+    partial class DodavanjeRadnje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxBrojIspostave = new System.Windows.Forms.TextBox();
+            this.textBoxAdresa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Broj ispostave";
+            // 
+            // textBoxBrojIspostave
+            // 
+            this.textBoxBrojIspostave.Location = new System.Drawing.Point(132, 24);
+            this.textBoxBrojIspostave.Name = "textBoxBrojIspostave";
+            this.textBoxBrojIspostave.Size = new System.Drawing.Size(105, 20);
+            this.textBoxBrojIspostave.TabIndex = 1;
+            // 
+            // textBoxAdresa
+            // 
+            this.textBoxAdresa.Location = new System.Drawing.Point(132, 64);
+            this.textBoxAdresa.Name = "textBoxAdresa";
+            this.textBoxAdresa.Size = new System.Drawing.Size(105, 20);
+            this.textBoxAdresa.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Adresa";
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(132, 105);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(105, 30);
+            this.buttonDodaj.TabIndex = 4;
+            this.buttonDodaj.Text = "Dodaj";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
+            // 
+            // DodavanjeRadnje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 144);
+            this.Controls.Add(this.buttonDodaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxAdresa);
+            this.Controls.Add(this.textBoxBrojIspostave);
+            this.Controls.Add(this.label1);
+            this.Name = "DodavanjeRadnje";
+            this.Text = "DodavanjeRadnje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxBrojIspostave;
+        private System.Windows.Forms.TextBox textBoxAdresa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDodaj;
+    }
+}
